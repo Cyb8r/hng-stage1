78518f597e6efb259d637d855da33f89d27ec7ed
cyb3rjay <cyb3rjay@outlook.com>
2024-06-28 01:56:42 -0500

    initial commit

---
+++ stage0/web/scripts.js
@@ -0,0 +1,8 @@
+// Example JavaScript to dynamically add more functionality if needed
+
+// Display an alert message when the page loads
+window.addEventListener('load', () => {
+    console.log('Welcome to My Simple Website!');
+});
+
+// Add more JS as needed to enhance interactivity
